fb0997ebcc2354fe7571578250a4ef1bcbe6077f
A5H-git <alvn.hendricks@gmail.com>
2024-03-03 19:58:49 -0800

    Updated to match colab notebook

--- src/watershed/watershed.py
+++ src/watershed/watershed.py
@@ -1,11 +1,11 @@
-import cv2
 import numpy as np
 from scipy.linalg import inv
 from scipy.ndimage import binary_fill_holes, distance_transform_edt
 from skimage import morphology
 from skimage.exposure import exposure
+from skimage.feature import peak_local_max
 from skimage.filters import threshold_otsu
-from skimage.measure import label, regionprops, regionprops_table
+from skimage.measure import label, regionprops
 from skimage.segmentation import watershed
 
 STAIN_VECTORS = {
@@ -31,10 +31,7 @@ INTENSITY_THRESHOLDS = {
     1: (0.6, 0.7),
 }
 
-SIZE_THRESHOLDS = {
-    0: (50, 1e3),
-    1: (50, 1e4),
-}
+SIZE_THRESHOLDS = {0: (50, 1e3), 1: (50, 1e4), 2: (100, 2500)}
 
 
 def _get_normalized_stain_vector(stain_vector):
@@ -61,55 +58,68 @@ def _threshold_and_binarize(
     image, intensity_thresh=(0.75, 0.85), size_thresh=(100, 2500)
 ):
     """Performs thresholding on the image."""
-    gray_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    adjusted_image = exposure.rescale_intensity(gray_img, in_range=intensity_thresh)
+    adjusted_image = exposure.rescale_intensity(image, in_range=intensity_thresh)
 
     bin_thresh = threshold_otsu(adjusted_image)
-    bin_img = binary_fill_holes(adjusted_image > bin_thresh)
+    filled_img = binary_fill_holes(adjusted_image > bin_thresh)
+
+    lbled_img = morphology.label(filled_img)
+    props = regionprops(lbled_img)
+
+    filtered_props = [
+        prop for prop in props if size_thresh[0] <= prop.area <= size_thresh[1]
+    ]
+
+    bin_mask = morphology.remove_small_objects(lbled_img)
 
     filtered_img = morphology.remove_small_objects(
-        bin_img, min_size=size_thresh[0], max_size=size_thresh[1]
+        bin_mask, min_size=size_thresh[0], max_size=size_thresh[1]
     )
 
     return filtered_img
 
 
-def _filter_img(bin_img, props_table, min_solidity, max_aspect_ratio):
+def _filter_img(bin_img, prop, min_solidity, max_aspect_ratio):
     """Filteres out `regionprops` based on solidity."""
     # Can be restructured if more filters are required.
 
-    props_table["LenWdRatio"] = (
-        props_table["major_axis_length"] / props_table["minor_axis_length"]
-    )
-    props_table["isGreater"] = props_table["LenWdRatio"] < max_aspect_ratio
-    remove_idxs = np.where(~props_table["isGreater"])[0]
-
-    for idx in remove_idxs:
-        slice_idx = props_table["subarray_idx"][idx]
-        bin_img[slice_idx[0], slice_idx[1]] = False
+    for prop in props:
+        aspect_ratio = prop.major_axis_length / prop.minor_axis_length
+        if aspect_ratio < max_aspect_ratio:
+            # Set the corresponding pixel values to False in the binary image
+            coords = prop.coords
+            bin_img[coords[:, 0], coords[:, 1]] = False
 
     labels = label(bin_img)
     props = regionprops(labels)
-    filtered_props = [prop.solidity for prop in props if prop.solidity >= min_solidity]
+    filtered_img = np.zeros_like(bin_img)
 
-    return filtered_props
+    for prop in props:
+        if prop.solidity >= min_solidity:
+            # Include the prop in the filtered binary image
+            filtered_img[labels == prop.label] = 1
+
+    return filtered_img
 
 
 def _watershed(bin_img, dist_thresh=2):
     """Watershed segmentation on a binary image."""
     # Perform distance transform
-    distances = -distance_transform_edt(~bin_img)
-    mask = morphology.h_minima(distances, dist_thresh)
-    masked_distances = np.minimum(distances, mask)
+    distances = distance_transform_edt(bin_img)
+
+    footprint_kernel = (3, 3)
+    coords = peak_local_max(
+        distances, footprint=np.ones(footprint_kernel), labels=bin_img
+    )
+
+    mask = np.zeros(distances.shape, dtype=bool)
+    mask[tuple(coords.T)] = True
 
     # Perform watershed
     markers = label(mask)
-    labels = watershed(masked_distances, markers, mask=bin_img)
+    segmented_img = watershed(-distances, markers, mask=bin_img)
 
-    segmented_img = bin_img.copy()
-    segmented_img[labels == 0] = 0
-
-    return labels, segmented_img
+    return segmented_img, distances
 
 
 REGION_PROP_KEYS = (
@@ -147,7 +157,7 @@ def generate_centroid(
         binary image of the segmented image.
     """
     # Preprocess the image
-    image = np.asfarray(image, np.float64)
+    image = np.asfarray(image, np.float64) / 255.0  # Normalize
     deconv_img = _deconvolve(image, stain_vector)
     hist_equalized_img = _hist_equalization(
         deconv_img[:, :, 1],
@@ -157,20 +167,13 @@ def generate_centroid(
 
     # Filter out noise (certain shapes)
     labels = label(bin_img)
-    props = regionprops_table(
-        labels,
-    )
-    filtered_props = _filter_img(labels, props, min_solidity, max_aspect_ratio)
-
-    # Generated the filtered binary image
-    bin_img = np.zeros_like(bin_img)
-
-    for prop in filtered_props:
-        bin_img[label == prop.label] = 1
+    props = regionprops(labels)
+    filtered_img = _filter_img(bin_img, props, min_solidity, max_aspect_ratio)
 
     # Generate promps from watershed
-    labels, img_mask = _watershed(bin_img, distance_thresh)
+    segmented_img, distances = _watershed(filtered_img, distance_thresh)
+    labels = label(segmented_img)
     props = regionprops(labels, coordinates="xy")
     centroid_coords = np.array([prop.centroid for prop in props]).astype(int)
 
-    return centroid_coords, img_mask, deconv_img
+    return centroid_coords, distances, deconv_img
