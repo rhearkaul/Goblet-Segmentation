6951ef812ade79d1d69bd435f7cbcde37239c25f
A5H-git <alvn.hendricks@gmail.com>
2024-03-22 15:56:57 -0700

    Updated get_prop doc and code

--- src/metrics.py
+++ src/metrics.py
@@ -12,36 +12,50 @@ PROPERTIES = [
 ]
 
 
-def get_prop(binary_mask):
+def get_prop(binary_mask: list, properties: list = PROPERTIES):
+    """
+    Gets the properties of the binary masks.
 
+    Parameters:
+    -----------
+    binary_mask: list
+        a collection of 2D images of binary masks.
+
+    properties: list
+        a collection of properties that is made available by
+        `skimage.measure.regionprops`.
+
+    Returns:
+    --------
+        pd.DataFrame of values representing each image as a row
+        and designated properties as columns.
+
+    Assuming we have .tiff we can convert the resolution to length
+
+    Example usage:
+    >>> from PIL import Image
+    >>> img = Image.open("image.tiff")
+    >>> res_x, res_y = img.info.get("resolution", (None, None))
+
+    >>> if not res_x or not res_y:
+        # Can be handled by default parameters as well...but assume
+    >>>     logging.error("Resolution not found, skipping image...")
+
+    >>> binary_masks = ...
+    >>> a_random_prop = get_prop(binary_masks[5])
+
+    >>> area = a_random_prop["area"] * res_x * res_y
+
+    >>> orientation = a_random_prop["orientation"]
+    >>> axis_major_length = np.linalg.norm([
+        a_random_prop["axis_major_length"] * np.cos(orientation) * res_x,
+        a_random_prop["axis_major_length"] * np.sin(orientation) * res_y,
+    ])
+    """
     labels = label(binary_mask)
-    props = regionprops_table(labels, properties=PROPERTIES)
+    props = regionprops_table(labels, properties=properties)
     df = pd.DataFrame(props)
 
     # Return the largest mask in case of artefacts
     i_max = df["area"].idxmax()
     return df.loc(i_max)
-
-
-## Assuming we have .tiff we can convert the resolution to length
-
-# from PIL import Image
-# img = Image.open("image.tiff")
-# res_x, res_y = img.info.get("resolution", (None, None))
-
-# if not res_x or not res_y:
-## Can be handled by default parameters as well...but assume
-# logging.error("Resolution not found, skipping image...")
-
-## Examples:
-# binary_masks = ...
-# a_random_prop = get_prop(binary_masks[5])
-#
-# area = a_random_prop["area"] * res_x * res_y
-#
-# orientation = a_random_prop["orientation"]
-# axis_major_length = np.linalg.norm([
-#   a_random_prop["axis_major_length"] * np.cos(orientation) * res_x,
-#   a_random_prop["axis_major_length"] * np.sin(orientation) * res_y,
-# ])
-#
